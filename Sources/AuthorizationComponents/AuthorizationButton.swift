b139b32fee4309bc62d14e973ee5660de0242142
ad <ad>
2024-03-28 13:46:07 +0600

    fixes

--- Sources/AuthorizationComponents/AuthorizationButton.swift
+++ Sources/AuthorizationComponents/AuthorizationButton.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
-//  
+//  AuthorizationButton.swift
+//
 //
 //  Created by Adlet Zhantassov on 28.03.2024.
 //
 
-import Foundation
+import UIKit
+
+public class AuthorizationButton: UIButton {
+    
+    public func configure(title: String, bgColor: String, fontName: String) {
+        setTitle(title, for: .normal)
+        backgroundColor = UIColor(named: bgColor)
+        titleLabel?.font = UIFont(name: fontName, size: 16)
+        setTitleColor(.white, for: .normal)
+        layer.cornerRadius = 12
+    }
+}
